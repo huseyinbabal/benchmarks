ddda557116eeac980a294b522a96588bfdba124c
Hüseyin BABAL <huseyinbabal88@gmail.com>
2026-02-09 11:53:59 +0300

    fix micrometer

--- fiber-vs-springboot/java-springboot/src/main/java/com/example/SpringBoot4ServerApplication.java
+++ fiber-vs-springboot/java-springboot/src/main/java/com/example/SpringBoot4ServerApplication.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.java21.instrument.VirtualThreadMetrics;
+import io.micrometer.java21.instrument.binder.jdk.VirtualThreadMetrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
